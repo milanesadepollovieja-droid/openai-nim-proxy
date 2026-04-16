65ab208eabb0e54a15a214ddf6302ef7b30ff9e9
milanesadepollovieja-droid <milanesadepollovieja@gmail.com>
2026-04-16 19:24:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const MODEL_MAPPING = {
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
-  'claude-3-opus': 'openai/gpt-oss-120b',
+  'claude-3-opus': 'z-ai/glm4.7',
   'claude-3-sonnet': 'deepseek-ai/deepseek-v3.2',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
 };
